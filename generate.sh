8904bcf6238943fd1501a00b606f69cfc3de1dac
Michael Steil <mist64@mac.com>
2021-05-27 03:23:44 +0200

    create HTML; include standard Darmok memes

--- generate.sh
+++ generate.sh
@@ -1,7 +1,14 @@
-rm -rf web
-mkdir web
-while IFS=":" read -r key value; do
+OUT=darmok
+
+rm -rf $OUT
+mkdir $OUT
+cat darmok.txt | grep -v "^#" | while IFS=":" read -r key value; do
 	echo $key
-	mkdir web/$key
-	meme-gen template.png web/${key}/${key}.jpg "$value"
-done < darmok.txt
+	value=$(echo $value | sed -e "s/^ *//")
+	mkdir $OUT/$key
+	meme-gen template.png $OUT/${key}/${key}.jpg "$value"
+	cat index.html | 
+		sed -e "s/KEY/$key/" |
+		sed -e "s/ALT/$value/" > $OUT/${key}/index.html
+		
+done
