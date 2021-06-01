61dd10ec10c22b5ee70f1eb00feaf712181b843d
Michael Steil <mist64@mac.com>
2021-06-01 12:34:36 +0200

    index; more originals

--- generate.sh
+++ generate.sh
@@ -2,16 +2,38 @@ OUT=darmok
 
 rm -rf $OUT
 mkdir $OUT
-cat darmok.txt | grep -v "^#" | while IFS=":" read -r key value; do
-	echo $key
+
+echo "<html>" > $OUT/index.html
+echo "<head>" >> $OUT/index.html
+echo "<meta property=\"og:title\" content=\"darmok.com\" />" >> $OUT/index.html
+echo "<meta property=\"og:image\" content=\"darmok/darmok.jpg\" />" >> $OUT/index.html
+echo "</head>" >> $OUT/index.html
+echo "<body>" >> $OUT/index.html
+
+
+cat darmok.txt | grep -v "^#" | while IFS=":" read -r key value link; do
 	value=$(echo $value | sed -e "s/^ *//")
+	link=$(echo $link | sed -e "s/^ *//")
+	link=${link//\//\\\/}
+	orig=$( (cd orig; ls ${key}.*) )
+	orig_extension="${orig##*.}"
+
+	echo $key
+
 	mkdir $OUT/$key
 	meme-gen template.png $OUT/${key}/${key}.jpg "$value"
+	cp orig/$orig $OUT/$key/orig.$orig_extension
 	cat index.html | 
 		sed -e "s/KEY/$key/" |
-		sed -e "s/ALT/$value/" > $OUT/${key}/index.html
+		sed -e "s/EXTENSION/$orig_extension/" |
+		sed -e "s/ALT/$value/" |
+		sed -e "s/LINK/$link/g" > $OUT/${key}/index.html
 
 	echo "<a href=\"$key\">" >> $OUT/index.html
-	echo "<img src=\"$key/${key}.jpg\" alt=\"$value\" width=\"160\">" >> $OUT/index.html
+	#echo "<img src=\"$key/${key}.jpg\" alt=\"$value\" width=\"160\">" >> $OUT/index.html
+	echo "<img src=\"$key/orig.${orig_extension}\" alt=\"$value\" width=\"160\">" >> $OUT/index.html
 	echo "</a>" >> $OUT/index.html
 done
+
+echo "</body>" >> $OUT/index.html
+echo "</html>" >> $OUT/index.html
